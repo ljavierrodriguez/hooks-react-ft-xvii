c653bf15a67f410344ad03f31273b5c0b30fc0aa
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2022-09-20 18:26:15 +0000

    Fixing Image Size

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -4,27 +4,30 @@ import ImageItem from './ImageItem';
 const Gallery = () => {
 
     const [images] = useState([
-        "https://picsum.photos/id/237/100/100",
-        "https://picsum.photos/id/500/100/100",
-        "https://picsum.photos/id/24/100/100",
-        "https://picsum.photos/id/989/100/100",
-        "https://picsum.photos/id/290/100/100",
-        "https://picsum.photos/id/650/100/100",
-        "https://picsum.photos/id/420/100/100",
-        "https://picsum.photos/id/120/100/100",
-        "https://picsum.photos/id/666/100/100",
-        "https://picsum.photos/id/123/100/100"
+        "https://picsum.photos/id/237/600/600",
+        "https://picsum.photos/id/500/600/600",
+        "https://picsum.photos/id/24/600/600",
+        "https://picsum.photos/id/989/600/600",
+        "https://picsum.photos/id/290/600/600",
+        "https://picsum.photos/id/650/600/600",
+        "https://picsum.photos/id/420/600/600",
+        "https://picsum.photos/id/120/600/600",
+        "https://picsum.photos/id/666/600/600",
+        "https://picsum.photos/id/123/600/600"
     ])
 
     let photoRef = useRef();
 
     const setImageSource = (source) => {
+        photoRef.current.style.width= "400px";
+        photoRef.current.style.height= "400px";
+        photoRef.current.style.objectFit= "cover";
         photoRef.current.src = source;
     }
 
     return (
         <>
-            <img className='w-50 my-2 mx-5 img-fluid' id="photo" ref={photoRef} />
+            <img className='my-2 mx-5 img-fluid' id="photo" ref={photoRef} />
             <div className="d-flex flex-wrap w-50 m-5">
                 {
                     images.map((source, index) => <ImageItem source={source} key={index} setImageSource={setImageSource} />)
